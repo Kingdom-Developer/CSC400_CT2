674f6f71f3d25a613f9a334a9b77df58c9c37323
Nolan Hill <nolanhill-developer@pm.me>
2025-11-22 16:05:12 -0700

    Modified main method to reflect requirements of the assignment.

--- CT2/src/com/CT2_Bag/Main.java
+++ CT2/src/com/CT2_Bag/Main.java
@@ -10,75 +10,43 @@ public class Main {
      * Main method that creates Bag data structure and calls on its methods
      */
     public static void main(String[] args) {
-        // Create an instance of the 'Bag' class
-        Bag<Integer> testBag = new Bag<Integer>();
-
-        // Add several elements to the bag, including duplicates
-        testBag.add(10);
-        testBag.add(40);
-        testBag.add(33);
-        testBag.add(15);
-        testBag.add(40);
-        testBag.add(33);
-        testBag.add(4);
-
-        // Print the bag contents
-        System.out.print("Items: ");
-
-        // Iterate through bag and print each item
-        for (Integer item : testBag) {
-            System.out.print(item + " ");
-        }
-        System.out.println();
-
-        // Test the 'contains' method for a few elements
-        System.out.println("Does the bag contain 15? " + testBag.contains(15));
-        System.out.println("Does the bag contain 4? " + testBag.contains(4));
-        System.out.println("Does the bag contain 7? " + testBag.contains(7));
-        System.out.println("Does the bag contain 33? " + testBag.contains(33));
-        System.out.println("Does the bag contain 1? " + testBag.contains(1));
-
-        // Test the 'count' method for a few elements
-        System.out.println("Number 10 appears " + testBag.count(10) + " times.");
-        System.out.println("Number 40 appears " + testBag.count(40) + " times.");
-        System.out.println("Number 1 appears " + testBag.count(1) + " times.");
-
-
+        // Create two instances of the "Bag" class
+        Bag<Integer> testBag1 = new Bag<Integer>();
         Bag<Integer> testBag2 = new Bag<Integer>();
-        testBag2.add(40);
-        testBag2.add(33);
-        testBag2.add(4);
-        System.out.println("Bag size is: " + testBag2.size());
-        System.out.println("Bag size is: " + testBag.size());
 
-        // Remove an element from the bag
-        testBag.remove(10);
-        testBag.remove(199); // Test removal of item that does not exist in the linked list
+        // Add items to testBag1, including duplicates
+        testBag1.add(10);
+        testBag1.add(20);
+        testBag1.add(10);
+        testBag1.add(40);
 
-        testBag.merge(testBag2);
+        // Add items to testBag2, including duplicates
+        testBag2.add(5);
+        testBag2.add(20);
+        testBag2.add(20);
 
+        // Print the size of each bag using the "size" method
+        System.out.println("The size of testBag1 is: " + testBag1.size());
+        System.out.println("The size of testBag2 is: " + testBag2.size());
 
-        // Print the bag contents again
-        System.out.print("Items: ");
+        // Merge the two bags together using the "merge" method
+        testBag1.merge(testBag2);
 
-        // Iterate through bag and print each item
-        for (Integer item : testBag) {
+        // Print the merged bag contents
+        System.out.print("Items of the merged bag: ");
+        for (Integer item : testBag1) {
             System.out.print(item + " ");
         }
         System.out.println();
 
-        // Iterate through bag and print each item
-        for (Integer item : testBag.distinct()) {
+        // Create a new bag containing only the distinct elements using the "distinct" method
+        Bag<Integer> distinctBag = testBag1.distinct();
+
+        // Print the distinct bag contents
+        System.out.print("Items of the distinct bag: ");
+        for (Integer item : distinctBag) {
             System.out.print(item + " ");
         }
         System.out.println();
-
-
-
-        // Test the 'contains' method for the removed element
-        System.out.println("Does the bag contain the removed value of 10? " + testBag.contains(10));
-
-        // Test the 'count' method for the removed element
-        System.out.println("The removed value of 10 appears " + testBag.count(10) + " times now.");
     }
 }
